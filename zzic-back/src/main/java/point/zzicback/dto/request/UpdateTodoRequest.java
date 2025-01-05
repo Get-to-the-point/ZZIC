57061cfbf73651a3be0024225eef59c74cc05d18
WorkaHolic <baileypark@kakao.com>
2025-01-05 23:58:55 +0900

    info: 애노테이션 추가

--- zzic-back/src/main/java/point/zzicback/dto/request/UpdateTodoRequest.java
+++ zzic-back/src/main/java/point/zzicback/dto/request/UpdateTodoRequest.java
@@ -1,17 +1,21 @@
 package point.zzicback.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 @Schema(description = "To-Do 업데이트 요청")
 public class UpdateTodoRequest {
+    @NotBlank
     @Schema(description = "To-Do 항목의 제목", example = "장보기 수정")
     private String title;
 
     @Schema(description = "To-Do 항목의 상세 설명", example = "우유, 빵, 계란, 치즈 구입")
     private String description;
 
+    @NotNull
     @Schema(description = "To-Do 항목의 완료 여부", example = "false")
     private Boolean done;
 }
